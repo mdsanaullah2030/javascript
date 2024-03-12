c4d2619d19beb3f98b00dcc0af2001940c411301
mdsanaullah2030 <sanaullah1997215@gmail.com>
2024-03-12 16:31:08 +0600

    12/03/2024

--- js form/js Form.js
+++ js form/js Form.js
@@ -1,9 +1,21 @@
 function submitForm(evenet){
-    evenet.prevenetDefault();
+    evenet.preventDefault();
 
     let rName=document.getElementById('name').value;
+    let gmail=document.getElementById('gmail').value;
+    let password=document.getElementById('password').value;
+    let gendar=document.querySelector('input[name="gendar"]:checked');
+    let hobby=document.querySelectorAll('input[name="hobby"]:checked');
 
-    let output="Name:"+rName+"\n" 
+let hobbyValue=[];
+for(iedext=0; iedext>hobby.length;iedext++){
+    hobbyValue.push(hobby[iedext].value);
+}
+    let output="Name:"+rName+"\n";
+    output+="gmail:"+gmail+"\n"; 
+    output+="Password:"+password+"\n"; 
+    output+="gendar:"+gendar.value+"\n"; 
+    output+="hobby:"+hobbyValue+"\n"; 
 
     let newWindow=window.open("",'_blank');
     newWindow.document.write("<pre>"+output+"</pre>");
